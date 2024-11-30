5349231fe4ad3eb9bdb2d4f2216ca3368f9c228f
modmuss50 <modmuss50@gmail.com>
2024-11-30 16:28:43 +0000

    Use ModifyExpressionValue

--- fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/mixin/datagen/DataCacheCachedDataMixin.java
+++ fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/mixin/datagen/DataCacheCachedDataMixin.java
@@ -20,27 +20,25 @@ import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.Map;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.hash.HashCode;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(targets = "net.minecraft.data.DataCache$CachedData")
 public abstract class DataCacheCachedDataMixin {
-	@WrapOperation(method = "write", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap;entrySet()Lcom/google/common/collect/ImmutableSet;"))
-	private ImmutableSet<Map.Entry<Path, HashCode>> sortPaths(ImmutableMap<Path, HashCode> instance, Operation<ImmutableSet<Map.Entry<Path, HashCode>>> original) {
-		return original.call(instance).stream()
+	@ModifyExpressionValue(method = "write", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap;entrySet()Lcom/google/common/collect/ImmutableSet;"))
+	private ImmutableSet<Map.Entry<Path, HashCode>> sortPaths(ImmutableSet<Map.Entry<Path, HashCode>> original) {
+		return original.stream()
 				.sorted(Map.Entry.comparingByKey(Comparator.comparing(k -> normalizePath(k.toString()))))
 				.collect(ImmutableSet.toImmutableSet());
 	}
 
-	@WrapOperation(method = "write", at = @At(value = "INVOKE", target = "Ljava/nio/file/Path;toString()Ljava/lang/String;"))
-	private String pathToString(Path instance, Operation<String> original) {
-		return normalizePath(original.call(instance));
+	@ModifyExpressionValue(method = "write", at = @At(value = "INVOKE", target = "Ljava/nio/file/Path;toString()Ljava/lang/String;"))
+	private String pathToString(String original) {
+		return normalizePath(original);
 	}
 
 	@Unique
