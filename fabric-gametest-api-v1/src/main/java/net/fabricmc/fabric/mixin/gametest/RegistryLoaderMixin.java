dd653275ffdff1d8037c366a3289bcc0a928968b
modmuss50 <modmuss50@gmail.com>
2025-01-21 18:39:39 +0000

    Checkstyle

--- fabric-gametest-api-v1/src/main/java/net/fabricmc/fabric/mixin/gametest/RegistryLoaderMixin.java
+++ fabric-gametest-api-v1/src/main/java/net/fabricmc/fabric/mixin/gametest/RegistryLoaderMixin.java
@@ -16,13 +16,10 @@
 
 package net.fabricmc.fabric.mixin.gametest;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import com.llamalad7.mixinextras.sugar.Local;
-import net.fabricmc.fabric.impl.gametest.FabricGameTestModInitializer;
-import net.minecraft.registry.DynamicRegistryManager;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.RegistryLoader;
-import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.resource.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,8 +27,13 @@ import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryLoader;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.resource.ResourceManager;
+
+import net.fabricmc.fabric.impl.gametest.FabricGameTestModInitializer;
 
 @Mixin(RegistryLoader.class)
 public class RegistryLoaderMixin {
